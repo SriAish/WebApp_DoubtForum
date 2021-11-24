00e59523478f7fc0fd03a3b586d83bcd48a23cf2
Aishwarya Srivastava <aishwaryasrivastava@Aishwaryas-MacBook-Air.local>
2021-11-24 15:20:58 +0530

    removed admin access to doubts

--- DoubtForum/admin.py
+++ DoubtForum/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-class DoubtAdmin(admin.ModelAdmin):
-    pass
-
-
 class SubjectAdmin(admin.ModelAdmin):
     pass
 
@@ -13,6 +9,5 @@ class SubjectAdmin(admin.ModelAdmin):
 class DoubtSessionAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(Doubt, DoubtAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(DoubtSession, DoubtSessionAdmin)
